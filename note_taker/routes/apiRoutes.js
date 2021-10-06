cfca8a6548b97bc11e94353f285d9d5b02bdba3e
Jared Minaga <jdm1code@gmail.com>
2021-10-05 21:42:19 -0600

    merged folder from sandbox

---
+++ note_taker/routes/apiRoutes.js
@@ -0,0 +1,22 @@
+const note_data = require("../db/note_data");
+//const router = require("express").Router();
+// exporting html paths
+module.exports = function(app) {
+// this gets the notes
+    app.get("/api/notes/", function(req,res) {
+        res.json(note_data);
+    });
+    
+    // adding notes
+    app.post("/api/notes/", function(req,res) {
+        note_data.push(req.body);
+        res.json(true);
+    })
+// this deletes notes
+    app.delete("/api/notes/", function(req,res) {
+        note_data.length = 0;
+
+        res.json({ok: true});
+    })
+
+};
