db2a124981bb47a9e4c44de466491fac932c3ae7
Jared Minaga <jdm1code@gmail.com>
2021-10-07 19:22:34 -0600

    API routes for creating .json with note data

--- note_taker/routes/apiRoutes.js
+++ note_taker/routes/apiRoutes.js
@@ -1,20 +1,45 @@
-const note_data = require("../db/note_data");
-//const router = require("express").Router();
+const fs = require("fs");
+
+
 // exporting html paths
 module.exports = function(app) {
-// this gets the notes
-    app.get("/api/notes/", function(req,res) {
-        res.json(note_data);
+// this adds notes to the json object
+app.get('/api/notes', function(req, res) {
+    fs.readFile('./db/db.json', (error, data) => {
+      notes = JSON.parse(data);
+      res.send(notes);
+      if (error) throw error;
     });
-    
+  });
+
     // adding notes
-    app.post("/api/notes/", function(req,res) {
-        note_data.push(req.body);
-        res.json(true);
-    })
-// this deletes notes
-    app.delete("/api/notes/", function(req,res) {
-        note_data.length = 0;
+    app.post('/api/notes', function(req, res) {
+        const addedNotes = req.body;
+    
+        fs.readFile('././db/db.json', (error, data) => {
+          notes = JSON.parse(data);
+          notes.push(addedNotes);
+          if (error) throw error;
+
+          let value = 1;
+          notes.forEach((note, index) => {
+            note.id = value;
+            value++;
+            return notes;
+          });
+          
+          makeString = JSON.stringify(notes);
+    
+          fs.writeFile('./db/db.json', makeString, (error, data) => {
+            if (error) throw error;
+          });
+        });
+        res.send();
+      });
+
+// to deletes notes
+    app.delete("/api/notes/id:", function(req,res) {
+        
 
         res.json({ok: true});
     })
--- note_taker/routes/htmlRoutes.js
+++ note_taker/routes/htmlRoutes.js
@@ -2,12 +2,12 @@ const path = require("path");
 
 module.exports = function(app) {
     
-    
+    //GET request for notes.jtml
     app.get("/notes", function(req,res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
-   
+   // default to index
    app.get("*", function(req,res) {
        res.sendFile(path.join(__dirname, "../public/index.html"));
      });
